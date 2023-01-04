e6954de3fbebf2ef8b28340e25cce6af4f20f4bd
naglepuff <mike.nagler@kitware.com>
2023-01-04 16:24:19 -0500

    Add workflow for adding categories

--- wsi_superpixel_guided_labeling/web_client/views/vue/components/ActiveLearningSetup/ActiveLearningSetupContainer.vue
+++ wsi_superpixel_guided_labeling/web_client/views/vue/components/ActiveLearningSetup/ActiveLearningSetupContainer.vue
@@ -1,27 +1,218 @@
 <script>
 import Vue from 'vue';
 import _ from 'underscore';
+import { restRequest } from '@girder/core/rest';
+import { ViewerWidget } from '@girder/large_image_annotation/views';
+
+const boundaryColor = 'rgba(0, 0, 0, 1)';
+const defaultCategory = {
+    label: 'default',
+    fillColor: 'rgba(0, 0, 0, 0)',
+    strokeColor: 'rbga(0, 0, 0, 1)'
+};
+
 
 export default Vue.extend({
     props: ['backboneParent', 'largeImageItem', 'superpixelAnnotation'],
     mounted() {
+        this.setupViewer();
+    },
+    created() {
+        this.debounceSaveAnnotation = _.debounce(this.saveAnnotation, 250);
     },
     data() {
         return {
-            categories: '',
+            categories: [],
+            categoryIndex: 0,
             radius: 100,
-            magnification: 5
+            magnification: 5,
+            viewerWidget: null,
+            overlayLayer: null,
+            superpixelElement: null,
+            pixelmapRendered: false,
+            currentCategoryLabel: 'New Category',
+            currentCategoryFillColor: 'rgba(255, 0, 0, 1)',
+            lastClickEventId: 0
         };
+    },
+    watch: {
+        currentCategoryLabel(newLabel, oldLabel) {
+            if (newLabel === oldLabel) {
+                return;
+            }
+            this.categories[this.categoryIndex]['category']['label'] = this.currentCategoryLabel;
+        },
+        currentCategoryFillColor(newColor, oldColor) {
+            if (newColor === oldColor) {
+                return;
+            }
+            this.categories[this.categoryIndex]['category']['fillColor'] = this.currentCategoryFillColor;
+        },
+        categoryIndex() {
+            this.currentCategoryLabel = this.categories[this.categoryIndex]['category']['label'];
+            this.currentCategoryFillColor = this.categories[this.categoryIndex]['category']['fillColor'];
+        },
+        categories: {
+            handler() {
+                const annotation = this.superpixelAnnotation.get('annotation');
+                if (!annotation || annotation.elements.length === 0) {
+                    return;
+                }
+                const superpixelElement = annotation.elements[0];
+                // this.saveAnnotation();
+                this._updatePixelmapLayerStyle();
+            },
+            deep: true
+        }
     },
     computed: {
         validForm() {
             return this.radius > 0 && this.magnification > 0;
+        },
+        validCategory() {
+            return true;
+        },
+        usingBoundaries() {
+            const superpixelElement = this.superpixelAnnotation.get('annotation').elements[0];
+            return superpixelElement.boundaries;
+        },
+        allNewCategories() {
+            const activeLearningCategories = _.map(this.categories, (category) => category.category);
+            return [defaultCategory, ...activeLearningCategories];
         }
     },
     methods: {
         generateInitialSuperpixels() {
-            console.log('generating initial set of superpixels');
             this.backboneParent.generateInitialSuperpixels(this.radius, this.magnification);
+        },
+        saveAnnotation() {
+            this.backboneParent._saveAnnotation();
+        },
+        createCategories() {
+            // TODO handle missing default, default in wrong position
+            const pixelmapElement = this.superpixelAnnotation.get('annotation').elements[0];
+            _.forEach(pixelmapElement.categories, (category, categoryIndex) => {
+                if (categoryIndex !== 0) {
+                    const labeledSuperpixels = _.chain(pixelmapElement.values)
+                        .map((value, valueIndex) => {
+                            value === categoryIndex ? valueIndex : 0;
+                        })
+                        .filter((value) => value !== 0)
+                        .result();
+                    this.categories.push({
+                        category: category,
+                        indices: labeledSuperpixels
+                    });
+                }
+            });
+            if(this.categories.length === 0) {
+                this.categories.push({
+                    category: {
+                        label: 'New Category',
+                        fillColor: 'rgba(255, 0, 0, 1)',
+                        strokeColor: boundaryColor
+                    },
+                    indices: []
+                });
+            }
+            this.categoryIndex = 0;
+            this.currentCategoryLabel = this.categories[0]['category']['label'];
+            this.currentCategoryFillColor = this.categories[0]['category']['fillColor'];
+        },
+        _addCategoryToAnnotation() {
+            const superpixelElement = this.superpixelAnnotation.get('annotation').elements[0];
+            superpixelElement.categories = this.allNewCategories;
+        },
+        _updatePixelmapLayerStyle() {
+            const categoryMap = this.allNewCategories;
+            const boundaries = this.usingBoundaries;
+            _.forEach(this.overlayLayer.features(), (feature) => {
+                feature.style('color', (d, i) => {
+                    if (d < 0 || d >= categoryMap.length) {
+                        console.warn(`No category found at index ${d} in the category map.`);
+                        return 'rgba(0, 0, 0, 0)';
+                    }
+                    const category = categoryMap[d];
+                    if (boundaries) {
+                        return (i % 2 === 0) ? category.fillColor : category.strokeColor;
+                    }
+                    return category.fillColor;
+                });
+            });
+            this.overlayLayer.draw();
+        },
+        _drawPixelmapAnnotation() {
+            this.viewerWidget.drawAnnotation(this.superpixelAnnotation);
+        },
+        _handlePixelmapClicked(overlayElement, overlayLayer, event) {
+            if (
+                this.lastClickEventId === event.eventID       // Duplicate event firing
+                || overlayElement.get('type') !== 'pixelmap'  // Not a pixelmap event
+                || !event.mouse.buttonsDown.left              // Not a left click
+            ) {
+                return;
+            }
+            this.lastClickEventId = event.eventID;
+
+            const index = overlayElement.get('boundaries') ? (event.index - event.index % 2) : event.index;
+            const offset = this.boundaries ? 1 : 0;
+            const data = this.overlayLayer.data();
+            // the +1 accounts for the default
+            const categoryIndex = data[index] === 0 ? this.categoryIndex + 1 : 0;
+            data[index] = data[index + offset] = categoryIndex;
+            // TODO: save new pixelmap value
+            this.overlayLayer.indexModified(index, index + offset).draw();
+            this.categories[this.categoryIndex].indices.push(index);
+        },
+        _onPixelmapRendered() {
+            this.pixelmapRendered = true;
+            this.createCategories();
+            this.viewerWidget.on('g:mouseClickAnnotationOverlay', this._handlePixelmapClicked);
+        },
+        _drawBaseImageLayer() {
+            if (this.viewerWidget) {
+                this.viewerWidget.destroy();
+            }
+            this.viewerWidget = new ViewerWidget.geojs({
+                parentView: this.backboneParent,
+                el: this.$refs.map,
+                itemId: this.largeImageItem.id,
+            });
+            this.viewerWidget.on('g:imageRendered', this._drawPixelmapAnnotation);
+            this.viewerWidget.on('g:drawOverlayAnnotation', (element, layer) => {
+                if (element.type === 'pixelmap') {
+                    this.overlayLayer = layer;
+                    this.superpixelElement = element;
+                    this._onPixelmapRendered();
+                }
+            });
+        },
+        setupViewer() {
+            if (!this.superpixelAnnotation) {
+                return;
+            }
+            restRequest({
+                url: `item/${this.largeImageItem.id}/tiles`
+            }).done((imageMetadata) => {
+                this._drawBaseImageLayer()
+            })
+        },
+        nextCategory() {
+            this.categoryIndex += 1;
+        },
+        previousCategory() {
+            this.categoryIndex -= 1;
+        },
+        addCategory() {
+            this.categories.push({
+                category: {
+                    label: 'New Category',
+                    fillColor: 'rgba(255, 0, 0, 1)',
+                    strokeColor: boundaryColor
+                },
+                indices: []
+            });
+            this.categoryIndex = this.categories.length - 1;
         }
     }
 });
@@ -65,7 +256,59 @@ export default Vue.extend({
             class="h-al-setup-categories"
             v-else
         >
-            <span>Setting up categories</span>
+            <div
+                v-if="pixelmapRendered"
+                class="h-add-new-category"
+            >
+                <div class="form-group">
+                    <label for="category-label">Label</label>
+                    <input
+                        id="category-label"
+                        class="form-control input-sm h-active-learning-input"
+                        v-model.lazy="currentCategoryLabel"
+                    >
+                </div>
+                <div class="form-group">
+                    <label for="fill-color">Fill Color</label>
+                    <input
+                        id="fill-color"
+                        class="form-control input-sm h-active-learning-input"
+                        v-model.lazy="currentCategoryFillColor"
+                    >
+                </div>
+                <button
+                    class="btn btn-primary h-previous-category-btn"
+                    :disabled="categoryIndex === 0"
+                    @click="previousCategory"
+                >
+                    Previous
+                </button>
+                <button
+                    class="btn btn-primary h-next-category-btn"
+                    :disabled="categoryIndex >= categories.length - 1"
+                    @click="nextCategory"
+                >
+                    Next
+                </button>
+                <button
+                    class="btn btn-primary h-add-category-btn"
+                    @click="addCategory"
+                >
+                    Add Category
+                </button>
+
+            </div>
+            <div class="h-setup-categories-body">
+                <div ref="map" class="h-setup-categories-map"></div>
+                <div class="h-category-setup-progress">
+                    <div
+                        v-for="(categoryLabels, index) in categories"
+                        :key="index"
+                    >
+                        {{ categoryLabels.category.label }}: {{ categoryLabels.indices }}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -73,11 +316,31 @@ export default Vue.extend({
 <style scoped>
 .h-active-learning-setup-container {
     margin-left: 10px;
+    width: 100%;
+    height: 100%;
+    position: absolute;
 }
 .h-active-learning-input {
     width: 30%;
 }
 .h-generate-superpixel-btn {
     display: block;
+}
+.h-al-setup-categories {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+}
+.h-setup-categories-body {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+}
+.h-setup-categories-map {
+    width: 50%;
+    height: 100%;
 }
 </style>
