06f3f5ca194a9e4233d9cc94c17cae7d8eade749
naglepuff <mike.nagler@kitware.com>
2023-01-16 17:09:22 -0500

    Add global KB shortcuts for navigation

--- wsi_superpixel_guided_labeling/web_client/views/vue/components/ActiveLearning/ActiveLearningKeyboardShortcuts.vue
+++ wsi_superpixel_guided_labeling/web_client/views/vue/components/ActiveLearning/ActiveLearningKeyboardShortcuts.vue
@@ -8,6 +8,47 @@ export default Vue.extend({
         categories() {
             return store.categories;
         }
+    },
+    methods: {
+        keydownListener(event) {
+            switch(event.key) {
+                case 'ArrowRight':
+                    if (store.selectedIndex === 7) {
+                        // TODO prevent going past the max
+                        store.page += 1;
+                        store.selectedIndex = 0;
+                    } else {
+                        store.selectedIndex++;
+                    }
+                    break;
+                case 'ArrowLeft':
+                    store.selectedIndex = store.selectedIndex === 0 ? 0 : store.selectedIndex - 1;
+                    if (store.selectedIndex === 0) {
+                        if (store.page !== 0) {
+                            store.selectedIndex = 7;
+                        }
+                        store.page = Math.max(0, store.page - 1);
+                    } else {
+                        store.selectedIndex--;
+                    }
+                    break;
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    //TODO implement category selection
+                default:
+                    break;
+            }
+        }
+    },
+    mounted() {
+        window.addEventListener('keydown', this.keydownListener);
     }
 });
 </script>
