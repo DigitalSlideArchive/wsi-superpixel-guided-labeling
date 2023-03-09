4b473f887e05854363d43e90d2926be5e56953db
naglepuff <mike.nagler@kitware.com>
2023-03-09 12:14:09 -0500

    Ensure the store categories has default

--- wsi_superpixel_guided_labeling/web_client/views/vue/components/ActiveLearning/ActiveLearningContainer.vue
+++ wsi_superpixel_guided_labeling/web_client/views/vue/components/ActiveLearning/ActiveLearningContainer.vue
@@ -178,7 +178,16 @@ export default Vue.extend({
         store.currentAverageConfidence = this.currentAverageConfidence;
 
         const predictionAnnotation = this.annotationsByImageId[this.selectedImageId].predictions;
-        store.categories = predictionAnnotation.get('annotation').elements[0].categories;
+        const predictionCategories = predictionAnnotation.get('annotation').elements[0].categories;
+        const nonDefaultPredictionsCategories = _.filter(predictionCategories, (category) => category.label !== 'default');
+        store.categories = [
+            {
+                label: 'default',
+                fillColor: 'rgba(0, 0, 0, 0)',
+                strokeColor: 'rgb(0, 0, 0)'
+            },
+            ...nonDefaultPredictionsCategories
+        ];
 
         const startIndex = 0;
         const endIndex = Math.min(startIndex + store.pageSize, this.sortedSuperpixelIndices.length);
