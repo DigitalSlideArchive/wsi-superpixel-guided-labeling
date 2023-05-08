7507763c481709978c0b3c9d4783f252a6b82ab2
naglepuff <mike.nagler@kitware.com>
2023-05-08 14:22:57 -0400

    Adjust HistomicsUI header

---
+++ wsi_superpixel_guided_labeling/web_client/views/HeaderImageView.js
@@ -0,0 +1,12 @@
+import { wrap } from '@girder/core/utilities/PluginUtils';
+import HeaderImageView from '@girder/histomicsui/views/layout/HeaderImageView.js';
+
+wrap(HeaderImageView, 'render', function (render) {
+    render.call(this);
+    const isActiveLearning = window.location.href.includes('active-learning');
+    if (isActiveLearning) {
+        this.$el.find('.h-open-annotated-image').remove();
+        this.$el.find('.h-open-image').remove();
+        this.$el.find('.h-links').remove();
+    }
+});
