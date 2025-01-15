e28a1fe12cf4ae2c2f3ad796cf3df31242e9029b
Brianna Major <brianna.major@taloid.khq.kitware.com>
2025-01-15 09:59:21 -0500

    Check config file to determine if Active Learning button is needed

--- wsi_superpixel_guided_labeling/web_client/views/itemList.js
+++ wsi_superpixel_guided_labeling/web_client/views/itemList.js
@@ -3,28 +3,45 @@ import ItemListWidget from '@girder/core/views/widgets/ItemListWidget';
 import _ from 'underscore';
 import { restRequest } from '@girder/core/rest';
 
+const specialFolders = ['Annotations', 'Models', 'Features'];
+
+/**
+ * When we view an item list, look for the histomicsui config file. Check that
+ * the activeLearning key is set to true and make sure there is at least one
+ * item present. If all criteria is met, add the "Active Learning" button.
+ */
 wrap(ItemListWidget, 'render', function (render) {
     render.call(this);
-    const activeLearningFolder = this.parentView.parentModel.get('meta').active_learning;
-    if (activeLearningFolder) {
-        const largeImageItems = _.filter(this.collection.models, (model) => model.attributes.largeImage);
-        // don't make the request if the button already exists
-        if (largeImageItems.length && !this.parentView.$el.find('.wsi-al-open').length) {
-            restRequest({
-                type: 'GET',
-                url: 'histomicsui/settings'
-            }).then((settings) => {
-                if (!this.parentView.$el.find('.wsi-al-open').length) {
-                    const webrootPath = settings['histomicsui.webroot_path'];
-                    const btnContainer = this.parentView.$el.find('.g-folder-header-buttons');
-                    btnContainer.prepend(
-                        `<a class="wsi-al-open btn btn-sm btn-primary" role="button" href="${webrootPath}#/active-learning?folder=${this.parentView.parentModel.id}" target="_blank">
-                            <i class="icon-link-ext"></i>Active Learning
-                        </a>`
-                    );
-                }
-                return 0;
-            });
-        }
+    const thisFolder = this.parentView.parentModel;
+    if (specialFolders.includes(thisFolder.attributes.name)) {
+        // Ignore the special Annotations, Models, and Features folders
+        return;
     }
+
+    restRequest({
+        url: `folder/${thisFolder.id}/yaml_config/.histomicsui_config.yaml`
+    }).done((config) => {
+        if (config && config['activeLearning'] || metaKeySet) {
+            const largeImageItems = _.filter(this.collection.models, (model) => model.attributes.largeImage);
+            // Don't make the request if the button already exists
+            if (largeImageItems.length && !this.parentView.$el.find('.wsi-al-open').length) {
+                restRequest({
+                    type: 'GET',
+                    url: 'histomicsui/settings'
+                }).then((settings) => {
+                    if (!this.parentView.$el.find('.wsi-al-open').length) {
+                        // Add the Active Learning button
+                        const webrootPath = settings['histomicsui.webroot_path'];
+                        const btnContainer = this.parentView.$el.find('.g-folder-header-buttons');
+                        btnContainer.prepend(
+                            `<a class="wsi-al-open btn btn-sm btn-primary" role="button" href="${webrootPath}#/active-learning?folder=${this.parentView.parentModel.id}" target="_blank">
+                                <i class="icon-link-ext"></i>Active Learning
+                            </a>`
+                        );
+                    }
+                    return 0;
+                });
+            }
+        }
+    });
 });
