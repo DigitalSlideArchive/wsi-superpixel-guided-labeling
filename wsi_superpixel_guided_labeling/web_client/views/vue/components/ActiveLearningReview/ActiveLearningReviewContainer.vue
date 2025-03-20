72d8ce6a824e0c6f127383d1479b35670e4e26e9
Brianna Major <brianna.major@kitware.com>
2025-03-20 17:06:59 -0400

    Handle changedSuperpixel when it is being removed
    A changedSuperpixel that already exists in the suprepixels for review needs to
    be removed. If the updated superpixel should still be displayed after all
    filters are applied it will be soreted and re-inserted in the correct location.

--- wsi_superpixel_guided_labeling/web_client/views/vue/components/ActiveLearningReview/ActiveLearningReviewContainer.vue
+++ wsi_superpixel_guided_labeling/web_client/views/vue/components/ActiveLearningReview/ActiveLearningReviewContainer.vue
@@ -722,45 +722,55 @@ export default Vue.extend({
                             acc.set(key, this.sortSuperpixels(value));
                             return acc;
                         }, new Map());
-                } else if (data) {
-                    const [group] = data.keys().toArray();
-                    const superpixels = this.filteredSortedGroupedSuperpixels.get(group);
-                    let index;
-                    const sortOrder = this.sortAscending ? 1 : -1;
-                    const changedImageName = this.imageItemsById[changedSuperpixel.imageId].name;
-                    switch (this.sortBy) {
-                        case 1:
-                            index = _.sortedIndex(superpixels, changedSuperpixel, (superpixel) => {
-                                const imageName = this.imageItemsById[superpixel.imageId].name;
-                                return imageName <= changedImageName * sortOrder;
-                            });
-                            break;
-                        case 2:
-                            index = _.sortedIndex(superpixels, changedSuperpixel, (superpixel) => {
-                                return superpixel.selectedCategory * sortOrder;
-                            });
-                            break;
-                        case 3:
-                            index = _.sortedIndex(superpixels, changedSuperpixel, (superpixel) => {
-                                return superpixel.prediction * sortOrder;
-                            });
-                            break;
-                        case 4:
-                            index = _.sortedIndex(superpixels, changedSuperpixel, (superpixel) => {
-                                return superpixel.confidence * sortOrder;
-                            });
-                            break;
-                        case 5:
-                            index = _.sortedIndex(superpixels, changedSuperpixel, (superpixel) => {
-                                return superpixel.certainty * sortOrder;
-                            });
-                            break;
-                        default:
-                            index = _.sortedIndex(superpixels, changedSuperpixel, (superpixel) => {
-                                return superpixel.index * sortOrder;
-                            });
+                } else {
+                    // Determine if the changed superpixel needs to be removed
+                    this.filteredSortedGroupedSuperpixels.forEach((values, key) => {
+                        const index = values.findIndex((v) => v.index === changedSuperpixel.index);
+                        if (index !== -1) values.splice(index, 1);
+                    });
+                    if (data.size) {
+                        const [group] = data.keys().toArray();
+                        const superpixels = this.filteredSortedGroupedSuperpixels.get(group);
+                        let index;
+                        const sortOrder = this.sortAscending ? 1 : -1;
+                        const changedImageName = this.imageItemsById[changedSuperpixel.imageId].name;
+                        switch (this.sortBy) {
+                            case 1:
+                                index = _.sortedIndex(superpixels, changedSuperpixel, (superpixel) => {
+                                    const imageName = this.imageItemsById[superpixel.imageId].name;
+                                    return imageName <= changedImageName * sortOrder;
+                                });
+                                break;
+                            case 2:
+                                index = _.sortedIndex(superpixels, changedSuperpixel, (superpixel) => {
+                                    return superpixel.selectedCategory * sortOrder;
+                                });
+                                break;
+                            case 3:
+                                index = _.sortedIndex(superpixels, changedSuperpixel, (superpixel) => {
+                                    return superpixel.prediction * sortOrder;
+                                });
+                                break;
+                            case 4:
+                                index = _.sortedIndex(superpixels, changedSuperpixel, (superpixel) => {
+                                    return superpixel.confidence * sortOrder;
+                                });
+                                break;
+                            case 5:
+                                index = _.sortedIndex(superpixels, changedSuperpixel, (superpixel) => {
+                                    return superpixel.certainty * sortOrder;
+                                });
+                                break;
+                            default:
+                                index = _.sortedIndex(superpixels, changedSuperpixel, (superpixel) => {
+                                    return superpixel.index * sortOrder;
+                                });
+                        }
+                        // Use "set" rather than just using "splice" so that we trigger the watcher
+                        // and update the displayed superpixels
+                        superpixels.splice(index, 0, changedSuperpixel);
                     }
-                    superpixels.splice(index, 0, changedSuperpixel);
+                    this.filteredSortedGroupedSuperpixels = new Map(this.filteredSortedGroupedSuperpixels);
                 }
                 if (!this.selectedSuperpixel && this.filteredSortedGroupedSuperpixels.has('data')) {
                     this.selectedSuperpixel = this.filteredSortedGroupedSuperpixels.get('data')[0];
