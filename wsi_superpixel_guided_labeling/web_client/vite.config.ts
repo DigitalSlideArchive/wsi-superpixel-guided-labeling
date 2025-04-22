d7da27ac5c183806beae96f4d4bf99410879551e
Brianna Major <brianna.major@kitware.com>
2025-04-22 09:19:04 -0400

    Replace webpack with vite

---
+++ wsi_superpixel_guided_labeling/web_client/vite.config.ts
@@ -0,0 +1,38 @@
+import { resolve } from 'path';
+
+import { defineConfig } from 'vite';
+import { compileClient } from 'pug';
+import vue from '@vitejs/plugin-vue2';
+
+function pugPlugin() {
+  return {
+    name: 'pug',
+    transform(src: string, id: string) {
+      if (id.endsWith('.pug')) {
+        return {
+          code: `${compileClient(src, {filename: id})}\nexport default template`,
+          map: null,
+        };
+      }
+    },
+  };
+}
+
+export default defineConfig({
+  plugins: [
+    pugPlugin(),
+    vue()
+  ],
+  build: {
+    sourcemap: !process.env.SKIP_SOURCE_MAPS,
+    lib: {
+      entry: resolve(__dirname, 'main.js'),
+      name: 'GirderPluginWSISuperpixelGuidedLabeling',
+      fileName: 'girder-plugin-wsi-superpixel-guided-labeling',
+    },
+  },
+  define: {
+    __BUILD_TIMESTAMP__: `${+new Date()}`,
+    'process.env': {},  // for legacy Vue 2
+  },
+});
